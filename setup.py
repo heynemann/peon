f9248cb23125a7836431ce6485f41dd427d4902f
Bernardo Heynemann <bernardo@corp.globo.com>
2009-12-09 18:01:45 -0200

    Added setup.py structure

---
+++ setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+# Copyright Bernardo Heynemann <heynemann@gmail.com>
+
+# Licensed under the Open Software License ("OSL") v. 3.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.opensource.org/licenses/osl-3.0.php
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'Peon',
+    version = "0.1.0",
+    description = "Peon is an auto-testing-like tool",
+    long_description = """Peon works for you while you are developing.""",
+    keywords = 'Testing Notify',
+    author = 'Bernardo Heynemann',
+    author_email = 'heynemann@gmail.com',
+    url = 'http://www.nonesofar.org',
+    license = 'OSI',
+    classifiers = ['Development Status :: 4 - Beta',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved',
+                   'Natural Language :: English',
+                   'Operating System :: MacOS',
+                   'Operating System :: Microsoft :: Windows',
+                   'Operating System :: POSIX :: Linux',
+                   'Programming Language :: Python :: 2.5',
+                   'Programming Language :: Python :: 2.6',
+                   'Topic :: Software Development :: Quality Assurance',
+                   'Topic :: Software Development :: Testing',],
+    install_requires=[
+    ],
+
+    entry_points = {
+        'console_scripts': [
+            'peon = peon.peon:main',
+        ],
+    },
+
+)
+
+
