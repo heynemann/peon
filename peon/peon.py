cf8f6462cdc25e5d6e75a08703ce06db1eb07f0e
Hugo Lopes Tavares <hltbra@gmail.com>
2010-05-21 08:58:20 -0300

    popping some mess out

--- peon/peon.py
+++ peon/peon.py
@@ -19,53 +19,73 @@
 # limitations under the License.
 
 import sys
-import glob,os,stat,time
+import glob
+import os
+import stat
+import time
 from os.path import abspath, dirname, join
+
 
 class Urgency(object):
     low = 0
     normal = 1
     critical = 2
 
+
+def _get_stats_from_filename(filename):
+    stats = os.stat(filename)
+    return stats[stat.ST_SIZE] + stats[stat.ST_MTIME]
+
+
 def _get_checksum_from_dir(dirpath, pattern):
     result = 0
     for file in glob.glob(os.path.join(dirpath, pattern)):
-        absoluteFileName = os.path.join(dirpath, file)
-        stats = os.stat(absoluteFileName)
-        result += stats[stat.ST_SIZE] + stats[stat.ST_MTIME]
+        absolute_filename = os.path.join(dirpath, file)
+        result += _get_stats_from_filename(absolute_filename)
     return result
 
-'''
-Watch for changes in all .py files. If changes, run nosetests. 
-'''
+
 def checkSumRecursive(directory, pattern='*.py'):
     result = 0
     for dirpath, dirs, files in os.walk(directory):
         result += _get_checksum_from_dir(dirpath, pattern)
     return result
 
+
 def main():
+    '''
+    Watch for changes in all files indicated by a glob pattern,
+    by default it looks for '*.py'.
+    If something has changes, run a given command or nosetests.
+    '''
     val = 0
     command = " ".join(sys.argv[1:]) or 'nosetests'
     is_build_broken = False
 
     try:
-        while (True):
+        while True:
             if checkSumRecursive('.') != val:
                 val = checkSumRecursive('.')
                 os.system('reset')
                 status = os.system(command)
                 if status != 0:
                     is_build_broken = True
-                    notify("Broken build", "Your command of '%s' returned exit code '%s'. Please verify the console output for more info." % (command, ret), "stop.png", urgency=Urgency.critical)
-                else:
-                    if is_build_broken:
-                        is_build_broken = False
-                        notify("Build fixed", "Your build with command of '%s' IS FIXED!" % (command), "tick.png")
+                    notify("Broken build",
+                            "Your command of '%s' returned exit"\
+                            "code '%s'. Please verify the console output for"\
+                            "more info." % (command, status),
+                            "stop.png",
+                            urgency=Urgency.critical)
+                elif is_build_broken:
+                    is_build_broken = False
+                    notify("Build fixed",
+                           "Your build with command '%s' IS FIXED!" % command,
+                           "tick.png")
 
             time.sleep(1)
     except KeyboardInterrupt:
         return
+
 
 def notify(title, message, image, urgency=Urgency.normal):
     try:
@@ -74,13 +94,15 @@ def notify(title, message, image, urgency=Urgency.normal):
         return
 
     urgencies = {
-        Urgency.low:pynotify.URGENCY_LOW,
-        Urgency.normal:pynotify.URGENCY_NORMAL,
-        Urgency.critical:pynotify.URGENCY_CRITICAL,
+        Urgency.low: pynotify.URGENCY_LOW,
+        Urgency.normal: pynotify.URGENCY_NORMAL,
+        Urgency.critical: pynotify.URGENCY_CRITICAL,
     }
 
     if pynotify.init("Nosy"):
-        n = pynotify.Notification(title, message, abspath(join(dirname(__file__), image)))
+        n = pynotify.Notification(title,
+                                  message,
+                                  abspath(join(dirname(__file__), image)))
         n.set_urgency(urgencies[urgency])
         n.show()
         time.sleep(2)
@@ -88,5 +110,3 @@ def notify(title, message, image, urgency=Urgency.normal):
 
 if __name__ == '__main__':
     sys.exit(main())
-
-
